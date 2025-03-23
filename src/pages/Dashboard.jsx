b2043b535ddcb5f1a4906923c583f4b02a552667
rahul <129116227+RahulSinghKathayat@users.noreply.github.com>
2025-03-23 10:52:03 +0530

    maa ksm final commit

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -10,7 +10,7 @@ export function Dashboard(){
         <div className="h-screen flex justify-center">
         <div className="flex justify-center w-full h-full bg-[#F3F3F3] [--color:#E1E1E1] [background-image:linear-gradient(0deg,transparent_24%,var(--color)_25%,var(--color)_26%,transparent_27%,transparent_74%,var(--color)_75%,var(--color)_76%,transparent_77%,transparent),linear-gradient(90deg,transparent_24%,var(--color)_25%,var(--color)_26%,transparent_27%,transparent_74%,var(--color)_75%,var(--color)_76%,transparent_77%,transparent)] [background-size:55px_55px]">
         
-        <div className="relative w-120 h-150 pt-5 pl-5 my-20 shadow-2xl bg-[#F7F8F9] rounded overflow-hidden flex flex-col items-left space-y-2">
+        <div className="relative w-120 h-150 pt-5 pl-5 pr-5 my-20 shadow-2xl bg-[#F7F8F9] rounded overflow-hidden flex flex-col items-left space-y-2">
             <div className="w-full"><Appbar /></div>
             <div className="relative inline-block">
                 <img src={girl} alt="girlImage"
